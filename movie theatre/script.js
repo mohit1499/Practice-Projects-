da3891bfd98cd8fad75695bc723b9e03a67fc904
MOHIT TAYAL <tayal.mohit14@gmail.com>
2021-07-18 14:50:08 +0530

    practice projects

---
+++ movie theatre/script.js
@@ -0,0 +1,29 @@
+const container=document.querySelector('.container');
+const seats= document.querySelectorAll('.row .seat:not(.occupied)');
+const count= document.querySelector('#count');
+const total =document.querySelector('#total');
+const movieSelect= document.getElementById('movie');
+
+let ticketPrice= +movieSelect.value;
+
+//update total and count
+function updateSelectedCount(){
+    const selectedSeats= document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsCount= selectedSeats.length;
+    count.innerText=selectedSeatsCount;
+    total.innerText=selectedSeatsCount*ticketPrice;
+}
+
+// movie select event
+    movieSelect.addEventListener('change',e=>{
+        ticketPrice = +e.target.value;
+        updateSelectedCount();
+    })
+
+//seat select event
+container.addEventListener('click',e=>{
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+        e.target.classList.toggle('selected');
+        updateSelectedCount();
+    }
+});
